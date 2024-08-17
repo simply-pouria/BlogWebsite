4b2a95bf4c74887185e1b47924b7fbd170d4d973
pouria :) <pouriapmd@gmail.com>
2024-08-17 20:09:02 +0330

    added basic views

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Article
+
+
+class BookListView(ListView):
+    model = Article
+    template_name = ''  # unfinished
+    context_object_name = 'articles'
+
+    def get_queryset(self):
+        # Order by published_date in descending order (most recent first)
+        # and limit the number of results to 10
+        return Article.objects.order_by('-created_at')[:10]
+
+
+class BookDetailView(DetailView):
+    model = Article
+    template_name = ''  # unfinished
+    context_object_name = 'articles'
+
+
+
 
 
 
