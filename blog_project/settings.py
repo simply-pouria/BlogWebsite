a989830aa34cc13717d5179129ecbd4006227777
pouria :) <pouriapmd@gmail.com>
2024-08-21 21:20:16 +0330

    added login
    also added login status in other templates

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+from django.urls import reverse
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = reverse('Home')
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog_app.urls')),  # Include blog_app's URLs with its namespace
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
