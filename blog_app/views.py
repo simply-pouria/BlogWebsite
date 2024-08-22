f046b334e32b0f6c88dc840e79275d5a294acf7f
pouria :) <pouriapmd@gmail.com>
2024-08-22 22:45:13 +0330

    added create and update views

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Article
 
 
@@ -18,6 +19,24 @@ class ArticleDetailView(DetailView):
     model = Article
     template_name = 'blog_app/article_detail.html'
     context_object_name = 'article'
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    fields = ["title", "body"]
+    template_name = 'blog_app/article_create.html'
+
+    def get_success_url(self):
+        return reverse_lazy('article_detail', kwargs={'pk': self.object.pk})
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ["title", "body"]
+    template_name = 'blog_app/article_update.html'
+
+    def get_success_url(self):
+        return reverse_lazy('article_detail', kwargs={'pk': self.object.pk})
 
 
 
