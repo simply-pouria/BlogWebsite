3c88cfd71a707b794b55b6b170bc3bf5d90b26db
pouria :) <pouriapmd@gmail.com>
2024-08-23 00:33:43 +0330

    added anchor tags for create & update

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from blog_app.views import ArticleListView, ArticleDetailView
+from blog_app.views import ArticleListView, ArticleDetailView, ArticleUpdateView, ArticleCreateView
 
 app_name = 'blog_app'
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', ArticleListView.as_view(), name='Home'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='ArticleView'),
+    path('article/new/', ArticleCreateView.as_view(), name='article_create'),
+    path('article/<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_update')
 ]
