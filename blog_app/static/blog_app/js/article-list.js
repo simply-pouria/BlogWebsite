a74abab2d893856274ed9b3b20b6ef566c2de0a3
pouria :) <pouriapmd@gmail.com>
2024-09-11 00:37:39 +0330

    stylized article_detail

--- blog_app/static/blog_app/js/article-list.js
+++ blog_app/static/blog_app/js/article-list.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
-  $('.sidenav').sidenav();
-  $('.scrollspy').scrollSpy();
-  $('.tooltipped').tooltip();
-});
+
+ $(document).ready(function () {
+   $('.sidenav').sidenav();
+   $('.scrollspy').scrollSpy();
+   $('.tooltipped').tooltip();
+ });
